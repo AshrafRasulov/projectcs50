eb2afef0ded0a127bb865416c047a3e4a5adaac2
AshrafRasulov <ashee87.news@gmail.com>
2024-04-20 16:33:23 +0500

    add-modules

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
-const { createServer } = require('node:http');
-const hostname = '127.0.0.1';
-const port = 3000;
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
-});
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+var express = require('express'),
+         db = require('mongojs'),
+ bodyParser = require('body-parser'),
+        app = express(),
+       PORT = 3000;
+
+// there is we will use static space
+
+app.use(express.static(__dirname + '/public')).listen(PORT);
+console.log("Go to Server: http://127.0.0.1:"+PORT);
